3dd3aefe4632d947cf8ea4338b59e9711539fd50
Gage Pickle <gagepickle@yahoo.com>
2020-09-10 19:49:06 -0500

    Add validation for required fields

--- client/scripts.js
+++ client/scripts.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector('nav')
+const inputs = document.querySelectorAll('input')
 
 document.querySelector('.fa-hamburger').addEventListener('click',
 // Callback function
@@ -6,3 +7,15 @@ document.querySelector('.fa-hamburger').addEventListener('click',
   // Short circuit or operator
     nav.classList.replace('is-collapsed', 'is-expanded') || nav.classList.replace('is-expanded', 'is-collapsed')
   })
+
+inputs.forEach(input => {
+  input.addEventListener('blur', (event) => {
+    const currentInput = event.target
+
+    if (currentInput.value) {
+      currentInput.classList.replace('is-success', 'is-error') || currentInput.classList.add('is-error')
+    } else {
+      currentInput.classList.replace('is-error', 'is-success') || currentInput.classList.add('is-success')
+    }
+  })
+})
