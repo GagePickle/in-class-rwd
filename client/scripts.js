bd5e788b1707d0ab14d4e002b93181e6ff825b91
Gage Pickle <gagepickle@yahoo.com>
2020-09-10 22:47:09 -0500

    Fix error in form

--- client/scripts.js
+++ client/scripts.js
@@ -13,8 +13,8 @@ inputs.forEach(input => {
     const currentInput = event.target
     const regex = new RegExp(currentInput.pattern)
 
-    // if 'not' true that regex passes or not true that there is a value
-    if (!regex.test(currentInput.value.toUpperCase()) || currentInput.value) {
+    // If 'not true' that regex passes or not true that there is a value
+    if (!regex.test(currentInput.value.toUpperCase()) || !currentInput.value) {
       currentInput.classList.replace('is-success', 'is-error') || currentInput.classList.add('is-error')
     } else {
       currentInput.classList.replace('is-error', 'is-success') || currentInput.classList.add('is-success')
