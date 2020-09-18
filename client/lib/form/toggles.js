76e0f899a83f3ac0a17ddce76660956651b0665b
Gage Pickle <gagepickle@yahoo.com>
2020-09-17 20:54:50 -0500

    Add filter function to separate 'non-speacial' fields

--- client/lib/form/toggles.js
+++ client/lib/form/toggles.js
@@ -2,6 +2,11 @@ const form = document.querySelector("form");
 
 const fields = form.querySelectorAll("div:not(:last-of-type)");
 
+// We need to seperate out the 'speacial' from the 'regular' fields
+const regularFields = [...fields].filter(
+  (field) => !field.hasAttribute("data-status")
+);
+
 export default () => {
-  console.log(fields);
+  console.log(fields, regularFields);
 };
